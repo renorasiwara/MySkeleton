9e820d0c6501042c3d45a141dfeadb7b2d7d0254
Reno Rasiwara <reno.rasiwara@muf.co.id>
2017-08-21 09:50:59 +0700

    update 8-20 global config

--- _nero/dev/application/core/frontend_controller.php
+++ _nero/dev/application/core/frontend_controller.php
@@ -6,8 +6,8 @@ class Frontend_Controller extends MY_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->helper(array());
-		$this->load->library(array());
-		$this->load->model(array('User_model'));
+		$this->load->library(array())
+;		$this->load->model(array('User_model'));
 
 		$this->site->side = 'frontend';
 		$this->site->template = 'default';
